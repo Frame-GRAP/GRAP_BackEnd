52f59bbf5a218f915f0367fdc1725475816f3a6c
sun <syoun602@gmail.com>
2021-05-03 00:28:34 +0900

    소셜 로그인(구글, 네이버, 카카오) 구현 완료

--- src/main/java/com/grap/user/domain/User.java
+++ src/main/java/com/grap/user/domain/User.java
@@ -31,15 +31,35 @@ public class User {
     @Column(length = 15, nullable = false)
     private String telephone;
 
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
     @CreationTimestamp
     private Timestamp createDate;
 
     @Builder
-    public User(String loginId, String password, String name, String telephone, Timestamp createDate) {
+    public User(String loginId, String password, String name, String telephone, String picture, Role role, Timestamp createDate) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.telephone = telephone;
+        this.picture = picture;
+        this.role = role;
         this.createDate = createDate;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
     }
 }
--- src/main/java/com/grap/user/repository/UserRepository.java
+++ src/main/java/com/grap/user/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.grap.user.repository;
 
 import com.grap.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLoginIdAndPassword(String loginId, String password);
+
+    Optional<User> findByLoginId(String loginId);
 
 //    @Query(value="SELECT * FROM User WHERE user_id = ?1 AND password = ?2", nativeQuery = true)
 //    User login(String user_id, String password);
