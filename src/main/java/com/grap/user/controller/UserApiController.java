d08866eca990152fc7492921b49fbf8cba8371af
yunha <a41212a@naver.com>
2021-05-10 03:20:25 +0900

    외부 로그인 방식 변경(스프링->리액트) 에 따른 API 작성

--- src/main/java/com/grap/user/controller/UserApiController.java
+++ src/main/java/com/grap/user/controller/UserApiController.java
@@ -3,10 +3,13 @@ package com.grap.user.controller;
 import com.grap.favor.dto.FavorListResponseDto;
 import com.grap.user.config.auth.LoginUser;
 import com.grap.user.config.auth.dto.SessionUser;
+import com.grap.user.dto.UserSaveRequestDto;
 import com.grap.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -15,5 +18,8 @@ import java.util.List;
 public class UserApiController {
     private final UserService userService;
 
-
+    @PostMapping("/api/user")
+    public Long save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
+        return userService.save(userSaveRequestDto);
+    }
 }
--- src/main/java/com/grap/user/service/UserService.java
+++ src/main/java/com/grap/user/service/UserService.java
@@ -4,11 +4,13 @@ import com.grap.favor.dto.FavorListResponseDto;
 import com.grap.favor.repository.FavorRepository;
 import com.grap.user.config.auth.dto.SessionUser;
 import com.grap.user.domain.User;
+import com.grap.user.dto.UserSaveRequestDto;
 import com.grap.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,7 +19,14 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final FavorRepository favorRepository;
+    private final HttpSession httpSession;
 
+    public Long save(UserSaveRequestDto userSaveRequestDto){
+        User user = userSaveRequestDto.toEntity();
+        httpSession.setAttribute("user", new SessionUser(user));
+
+
+        return userRepository.save(user).getId();
+    }
 }
 
