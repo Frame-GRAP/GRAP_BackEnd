06a50b1a5882cd0bdbd6a3f227664bb1ca757efc
sun <syoun602@gmail.com>
2021-05-18 18:45:59 +0900

    헤더 이미지 업로드 구현

--- src/main/java/com/grap/user/config/WebConfig.java
+++ src/main/java/com/grap/user/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.grap.user.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,6 +17,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
+    }
 
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/header-img/**")
+                .addResourceLocations("classpath:/static/header-img/");
     }
 }
